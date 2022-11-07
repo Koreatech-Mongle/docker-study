0be6cc46868abb8b369c6b5d13006657ec3a0408
GyuBa <sansam1010@naver.com>
2022-11-08 03:50:12 +0900

    Images

---
+++ Example/Image/importDockerImages.js
@@ -0,0 +1,10 @@
+let Docker = require('dockerode')
+let docker = new Docker({host:'abstr.net', port:30001});
+
+docker.importImage("export.tar", {repo:"sansam1010/testimage"})
+    .then(function (data) {
+        console.log(data)
+    })
+    .catch(function (err){
+        console.log(err)
+    })
