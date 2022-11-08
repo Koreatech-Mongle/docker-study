39c07eca0b470bca5782050c0d314d8b956b05d8
refracta <58779799+refracta@users.noreply.github.com>
2022-11-09 04:56:41 +0900

    Minor fix

--- Example/docker-run.js
+++ Example/docker-run.js
@@ -5,9 +5,10 @@ let docker = new Docker({host: 'abstr.net', port: 30001});
 let imageName = 'sansam1010/testimage';
 
 let image = docker.getImage(imageName);
+console.log(image);
 
 try {
-    let data = await docker.run(imageName, ['/bin/bash'], process.stdout);
+    let data = await docker.run(imageName, ['/bin/bash'], process.stdout, {}, {}, _ => _);
     console.log('LOG', data);
 } catch (e) {
     console.error('ERROR', e);
